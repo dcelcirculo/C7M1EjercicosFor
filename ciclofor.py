c9e68368ba009a064d5eedc284f6fe0ec19d6d70
CaroGil <dcgil83@gmail.com>
2026-03-11 18:45:26 -0700

    Arreglo para que imprima el titulo de la tabla antes de iniciar la tabla y no a lo último

--- ciclofor.py
+++ ciclofor.py
@@ -2,7 +2,7 @@
 tabla = 1
 
 for x in range(1,11):
+    print(f"Tabla del {x}")
     for y in range(1, 11):
         print(f"{y} * {tabla} = {y * tabla}")
-    print(f"Tabla del {x}")
     tabla += 1
